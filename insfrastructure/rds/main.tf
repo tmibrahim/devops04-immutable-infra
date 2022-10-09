191c4fc1637b59c763b5ca98ff135b0a2496c1d4
tmibrahim <ibrahim-temur@outlook.com>
2022-10-09 22:14:12 +0000

    Update sg for RDS

--- insfrastructure/rds/main.tf
+++ insfrastructure/rds/main.tf
@@ -12,6 +12,6 @@ resource "aws_db_instance" "default" {
     username = "postgres"
     password = var.password
     identifier = var.id
-    # pc_security_group_ids = [ "vpc-0aea46b717f801cc7" ]
+    pc_security_group_ids = ["sg-0b02c1644c6d36c3a"]
     publicly_accessible = true
 }
